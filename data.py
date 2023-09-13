35c690ca7830d8411f2a8949cf0b098fd7016b35
Alex Wayne <alex.wayne.dev@gmail.com>
2023-09-13 07:04:33 -0700

    wiki scraping / routing working

--- data.py
+++ data.py
@@ -1,5 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
+from models import Language, State
+from db import db
 
 def get_list_of_states():
     url = "https://en.wikipedia.org/wiki/List_of_sovereign_states"
@@ -25,7 +27,58 @@ def get_list_of_states():
 
     return states
 
-def get_official_languages(state):
+def is_valid_language(language):
+    cleaned = clean_language(language).lower()
+    
+    if "languages" in cleaned:
+        return False
+    if "others" in cleaned:
+        return False
+    if "federal level" in cleaned:
+        return False
+    if cleaned == "see full list": 
+        return False
+    return True
+
+def check_row_for_languages(row):
+    header = row.find("th")
+    data = row.find("td")
+    if header and header.get_text():
+        if data and not isinstance(data, int):
+            header_text = header.get_text().lower()
+            if "languages" in header_text:
+                list = data.find_all('a')
+                languages = []
+                for item in list:
+                    if item.parent.name == 'i':
+                        continue
+
+                    if item.has_attr('title'):
+                        cleaned = clean_language(item.get_text())
+                        if is_valid_language(cleaned):
+                            languages.append(cleaned)
+
+                return languages
+            elif "language" in header_text:
+                cleaned = clean_language(data.get_text())
+                if is_valid_language(cleaned):
+                    return [cleaned]
+                else:
+                    return []
+            
+    return None
+
+
+def clean_language(language):
+    if '[' in language:
+        language = language[:language.index('[')]
+    if '(' in language:
+        language = language[:language.index('(')]
+    if '\n' in language:
+        language = language[0:language.index('\n')] + language[language.index('\n') + 1:]
+    return language.strip()
+
+def get_languages(state):
     url = f"https://en.wikipedia.org/wiki/{state}"
 
     response = requests.get(url)
@@ -37,13 +90,19 @@ def get_official_languages(state):
 
     soup = BeautifulSoup(html, 'html.parser')
 
-    table = soup.find_all("table")[1]
+    table = soup.find_all('table', 'vcard')
+    if not table:
+        if state.endswith('country'):
+            return None
+        else:
+            return get_languages(state + " (country)")
+    table = table[0].find('tbody')
     rows = table.find_all('tr')
+    languages = []
     for row in rows:
-        print(row.prettify())
-        header = row.find("th", "infobox-label")
-        if header and header.get_text() in "Official language":
-            print("FOUND OFFICIAL LANGUAGE")
-            print(row.find("td", "infobox-data"))
-            break
-    return None
+        new_languages = check_row_for_languages(row)
+        if new_languages:
+            languages.extend(new_languages)
+        
+    return languages
+
--- models.py
+++ models.py
@@ -3,9 +3,27 @@ from db import db
 class State(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
+    languages = db.relationship('Language', secondary='state_language', lazy='subquery', back_populates='states')
     created_at = db.Column(db.DateTime, nullable=False, default=db.func.now())
     updated_at = db.Column(db.DateTime, nullable=False, default=db.func.now(), onupdate=db.func.now())
+
+    def __init__(self, name) -> None:
+        super().__init__()
+        self.name = name
 
     def __str__(self):
         return self.name
     
+class Language(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    states = db.relationship('State', secondary='state_language', lazy='subquery', back_populates='languages')
+    created_at = db.Column(db.DateTime, nullable=False, default=db.func.now())
+    updated_at = db.Column(db.DateTime, nullable=False, default=db.func.now(), onupdate=db.func.now())
+
+    def __init__(self, name) -> None:
+        super().__init__()
+        self.name = name
+
+    def __str__(self):
+        return self.name
